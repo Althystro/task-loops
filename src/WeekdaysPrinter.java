3ea3abbace488dec3eed2f4b97a93ab8ed7b42e3
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2024-11-04 10:25:31 +0300

    done

---
+++ src/WeekdaysPrinter.java
@@ -0,0 +1,23 @@
+public class WeekdaysPrinter {
+
+    public static void main(String[] args) {
+
+        String[] weekdays = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",};
+
+        System.out.println(weekdays);
+
+
+        for (int i=0;i<weekdays.length;++i ){
+            if (weekdays[i]=="Friday") {
+                break;
+            }
+            System.out.println(weekdays[i]);
+
+
+
+        }
+//
+//
+//
+    }}
+
